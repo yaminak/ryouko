5e5b0a89f495ed9eafb060363faf5cf1c5da0e5b
yaminakara <yamina.karamane@lepoles.org>
2022-11-29 16:34:18 +0100

    ryouko suite 9

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use App\Entity\Categorie;
 use App\Repository\HobbiesRepository;
 use App\Repository\CategorieRepository;
+use App\Repository\PhotosRepository;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -15,7 +16,7 @@ class HomeController extends AbstractController
      * @Route("/", name="app_home")
      */
     public function index(CategorieRepository $cr): Response
-    {
+    { 
         $categorie = $cr->findOneByCategorieField('home');
         return $this->render('home/index.html.twig', [
             'categorie' => $categorie,
